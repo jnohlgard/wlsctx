5aeda7463efd4755058e8ec9f29e9a6b5c21f8ac
Joakim Nohlgård <joakim@nohlgard.se>
2025-10-02 15:21:15 +0200

    First working implementation

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
-use log::{debug, error, info};
+use log::{debug, error, info, warn, };
 
 use wayland_client::{
     delegate_noop,
-    protocol::{
-        wl_registry,
-    },
-    Connection, Dispatch, QueueHandle
+    protocol::wl_registry,
+    Connection, QueueHandle,
+    globals::{registry_queue_init, GlobalListContents},
 };
 use wayland_protocols::wp::security_context::v1::client::{
     wp_security_context_manager_v1,
@@ -16,19 +15,17 @@ use std::os::unix::{
     fs::FileTypeExt,
     net::UnixListener,
 };
-use std::os::fd::{
-    AsFd,
-    OwnedFd,
-    FromRawFd,
-};
+use std::os::fd::{ AsFd, FromRawFd, OwnedFd, };
 use std::io;
 use std::fs;
 use std::path;
 use sd_notify;
 
-extern crate pretty_env_logger;
+use env_logger::Env;
 
 use clap::Parser;
+use signal::trap::Trap;
+use signal::Signal::{SIGHUP, SIGTERM, SIGINT};
 
 /// Set up a Wayland socket with an attached security context
 ///
@@ -52,72 +49,32 @@ struct Cli {
     listen: Option<path::PathBuf>,
 }
 
-// This struct represents the state of our app. This simple app does not
-// need any state, by this type still supports the `Dispatch` implementations.
-#[derive(Debug)]
-struct State {
-    app_id: String,
-    instance_id: String,
-    sandbox_engine: String,
-    listen_fd: OwnedFd,
-    close_fd: Option<OwnedFd>,
-    needs_roundtrip: bool,
-}
+struct State;
 
-// Implement `Dispatch<WlRegistry, ()> for out state. This provides the logic
-// to be able to process events for the wl_registry interface.
-//
-// The second type parameter is the user-data of our implementation. It is a
-// mechanism that allows you to associate a value to each particular Wayland
-// object, and allow different dispatching logic depending on the type of the
-// associated value.
-//
-// In this example, we just use () as we don't have any value to associate. See
-// the `Dispatch` documentation for more details about this.
-impl Dispatch<wl_registry::WlRegistry, ()> for State {
+impl wayland_client::Dispatch<wl_registry::WlRegistry, GlobalListContents> for State {
     fn event(
-        state: &mut Self,
-        registry: &wl_registry::WlRegistry,
-        event: wl_registry::Event,
-        _userdata: &(),
+        _state: &mut Self,
+        _proxy: &wl_registry::WlRegistry,
+        _event: wl_registry::Event,
+        // This mutex contains an up-to-date list of the currently known globals
+        // including the one that was just added or destroyed
+        _data: &GlobalListContents,
         _conn: &Connection,
-        qh: &QueueHandle<Self>,
+        _qhandle: &QueueHandle<Self>,
     ) {
-        // When receiving events from the wl_registry, we are only interested in the
-        // `global` event, which signals a new available global.
-        if let wl_registry::Event::Global { name, interface, version } = event {
-            match &interface[..] {
-                "wp_security_context_manager_v1" => {
-                    debug!("Wayland security context manager protocol {interface} (v{version})");
-                    let security_context_manager = registry.bind::<wp_security_context_manager_v1::WpSecurityContextManagerV1, _, _>(name, wp_security_context_manager_v1::REQ_CREATE_LISTENER_SINCE, qh, ());
-                    let (reader, writer) = io::pipe().unwrap();
-                    let security_context = security_context_manager.create_listener(state.listen_fd.as_fd(), reader.as_fd(), qh, ());
-                    state.close_fd = Some(writer.into());
-                    let app_id = &state.app_id;
-                    let instance_id = &state.instance_id;
-                    info!("Create security context mapping for {app_id} ({instance_id})");
-                    security_context.set_sandbox_engine(state.sandbox_engine.clone());
-                    security_context.set_app_id(app_id.clone());
-                    security_context.set_instance_id(instance_id.clone());
-                    security_context.commit();
-                    security_context.destroy();
-                    state.needs_roundtrip = true;
-                },
-                _ => {
-                    debug!("[{name}] {interface} (v{version})");
-                }
-            }
-        }
+        /* react to dynamic global events here */
     }
 }
 
 // The security context protocol has no events that we need to manage
-delegate_noop!(State: ignore wp_security_context_manager_v1::WpSecurityContextManagerV1);
-delegate_noop!(State: ignore wp_security_context_v1::WpSecurityContextV1);
+delegate_noop!(State: wp_security_context_manager_v1::WpSecurityContextManagerV1);
+delegate_noop!(State: wp_security_context_v1::WpSecurityContextV1);
+delegate_noop!(State: ignore wl_registry::WlRegistry);
 
 // The main function of our program
 fn main() {
-    pretty_env_logger::init();
+    let env = Env::default().default_filter_or("warn");
+    env_logger::init_from_env(env);
     let cli = Cli::parse();
 
     let listener = match (cli.socket_activation, cli.listen) {
@@ -161,60 +118,49 @@ fn main() {
         _ => (),
     };
 
-
-    let mut state = State{
-        sandbox_engine: cli.sandbox_engine,
-        app_id: cli.app_id,
-        instance_id: cli.instance_id,
-        listen_fd: listener.into(),
-        close_fd: None,
-        needs_roundtrip: true,
+    let close_fd: OwnedFd = {
+        // Create a Wayland connection by connecting to the server through the
+        // environment-provided configuration.
+        let conn = Connection::connect_to_env().expect("upstream Wayland connection failed");
+        let (globals, mut event_queue) = registry_queue_init::<State>(&conn).unwrap();
+        let qh = &event_queue.handle();
+        let security_context_manager: wp_security_context_manager_v1::WpSecurityContextManagerV1 = globals.bind(qh, 1..=1, ()).unwrap();
+        let (reader, writer) = io::pipe().unwrap();
+        let security_context = security_context_manager.create_listener(listener.as_fd(), reader.as_fd(), qh, ());
+        security_context_manager.destroy();
+        let sandbox_engine = &cli.sandbox_engine;
+        let app_id = &cli.app_id;
+        let instance_id = &cli.instance_id;
+        info!("Create security context mapping for {app_id} ({instance_id})");
+        security_context.set_sandbox_engine(sandbox_engine.clone());
+        security_context.set_app_id(app_id.clone());
+        security_context.set_instance_id(instance_id.clone());
+        security_context.commit();
+        security_context.destroy();
+        event_queue.roundtrip(&mut State{}).unwrap();
+        writer.into()
     };
-    // Create a Wayland connection by connecting to the server through the
-    // environment-provided configuration.
-    let conn = Connection::connect_to_env().expect("upstream Wayland connection failed");
-
-    // Retrieve the WlDisplay Wayland object from the connection. This object is
-    // the starting point of any Wayland program, from which all other objects will
-    // be created.
-    let display = conn.display();
-
-    // Create an event queue for our event processing
-    let mut event_queue = conn.new_event_queue();
-    // And get its handle to associated new objects to it
-    let qh = event_queue.handle();
-
-    // Create a wl_registry object by sending the wl_display.get_registry request
-    // This method takes two arguments: a handle to the queue the newly created
-    // wl_registry will be assigned to, and the user-data that should be associated
-    // with this registry (here it is () as we don't need user-data).
-    let _registry = display.get_registry(&qh, ());
-
-    // At this point everything is ready, and we just need to wait to receive the events
-    // from the wl_registry.
-
-    // To actually receive the events, we invoke the `sync_roundtrip` method. This method
-    // is special and you will generally only invoke it during the setup of your program:
-    // it will block until the server has received and processed all the messages you've
-    // sent up to now.
-    //
-    // In our case, that means it'll block until the server has received our
-    // wl_display.get_registry request, and as a reaction has sent us a batch of
-    // wl_registry.global events.
-    //
-    // `sync_roundtrip` will then empty the internal buffer of the queue it has been invoked
-    // on, and thus invoke our `Dispatch` implementation that prints the list of advertized
-    // globals.
-    while state.needs_roundtrip {
-        state.needs_roundtrip = false;
-        event_queue.roundtrip(&mut state).unwrap();
+    let trap = Trap::trap(&[SIGTERM, SIGINT, SIGHUP]);
+    info!("Holding close_fd open to keep the tagged wayland socket available {close_fd:?}");
+    for sig in trap {
+        debug!("Signal: {sig:#?}");
+        match sig {
+            SIGINT => {
+                println!("Caught CTRL+C, stopping...");
+                break;
+            },
+            SIGTERM => {
+                debug!("Stopping");
+                break;
+            },
+            SIGHUP => {
+                warn!("TODO: SIGHUP restart");
+                break;
+            },
+            sig => {
+                warn!("Unexpected signal {sig:#?}");
+            },
+        }
     }
-    match &state.close_fd {
-        Some(fd) => {
-            println!("Listen fd: {fd:?}");
-        },
-        _ => {
-            println!("No listen_fd");
-        },
-    }
+    info!("Shutting down...");
 }
